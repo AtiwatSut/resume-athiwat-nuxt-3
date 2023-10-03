7ff16ba339d32b14f10f6942b6914a9a8c05b03b
AtiwatSut <135593013+AtiwatSut@users.noreply.github.com>
2023-10-03 16:40:44 +0700

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="wrapper">
-    <slot />
+    <v-container class="pa-0 py-md-0 container">
+      <slot />
+    </v-container>
   </div>
 </template>
 
@@ -10,4 +12,5 @@
 
 <style lang="scss" scoped>
 .wrapper { min-height: 100vh; box-sizing: border-box; }
+.container { max-width: 840px !important; }
 </style>
